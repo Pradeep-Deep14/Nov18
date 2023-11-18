cc91d1bb382a476cba48017bb76ae6a01d4ec4d6
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-11-18 12:10:01 +0530

    first commit

---
+++ 4.py
@@ -0,0 +1,10 @@
+def mul(x=10,y):
+    return (x*y)
+print(mul(5,15))
+
+
+#when defining a function with default
+#  parameter values, 
+# the parameters with default
+#  values must come after parameters
+#  without default values
